34d5e92cbff1a538e2d8e463e71aab4ea79b01d6
ayan4792 <nahouyabhalobasha@gmail.com>
2025-03-18 05:46:01 +0600

    Update autoreact.js

--- scripts/cmds/autoreact.js
+++ scripts/cmds/autoreact.js
@@ -11,17 +11,17 @@ module.exports = {
     },
 	onStart: async function (){},
 	onChat: async function ({ event ,api}) {
-		if (event.body.toLowerCase().indexOf("iloveyou") !== -1) return api.setMessageReaction("😙", event.messageID,event.threadID)
+		if (event.body.toLowerCase().indexOf("🙂") !== -1) return api.setMessageReaction("😙", event.messageID,event.threadID)
 		
-		if (event.body.toLowerCase().indexOf("good night") !== -1) return api.setMessageReaction("💗", event.messageID,event.threadID)
+		if (event.body.toLowerCase().indexOf("good night") !== -1) return api.setMessageReaction("🥱", event.messageID,event.threadID)
 		
-		if (event.body.toLowerCase().indexOf("good morning") !== -1) return api.setMessageReaction("💗", event.messageID,event.threadID)
+		if (event.body.toLowerCase().indexOf("good morning") !== -1) return api.setMessageReaction("🥱", event.messageID,event.threadID)
 		
-		if (event.body.toLowerCase().indexOf("pakyo") !== -1) return api.setMessageReaction("😠", event.messageID,event.threadID)
+		if (event.body.toLowerCase().indexOf("tom") !== -1) return api.setMessageReaction("😍", event.messageID,event.threadID)
 		
-		if (event.body.toLowerCase().indexOf("mahal") !== -1) return api.setMessageReaction("💗", event.messageID,event.threadID)
+		if (event.body.toLowerCase().indexOf("😑") !== -1) return api.setMessageReaction("💗", event.messageID,event.threadID)
 		
-		if (event.body.toLowerCase().indexOf("mwa") !== -1) return api.setMessageReaction("💗", event.messageID,event.threadID)
+		if (event.body.toLowerCase().indexOf("🫠") !== -1) return api.setMessageReaction("💗", event.messageID,event.threadID)
 		
 		if (event.body.toLowerCase().indexOf("😢") !== -1) return api.setMessageReaction("😢", event.messageID,event.threadID)
 		
@@ -37,31 +37,31 @@ module.exports = {
 
 		if (event.body.toLowerCase().indexOf("huss") !== -1) return api.setMessageReaction("🙃", event.messageID,event.threadID)
 
-		if (event.body.toLowerCase().indexOf("gago") !== -1) return api.setMessageReaction("😡", event.messageID,event.threadID)
+		if (event.body.toLowerCase().indexOf("cup") !== -1) return api.setMessageReaction("🤫", event.messageID,event.threadID)
 
-    		if (event.body.toLowerCase().indexOf("bastos") !== -1) return api.setMessageReaction("😳", event.messageID,event.threadID)
+    		if (event.body.toLowerCase().indexOf("🥵") !== -1) return api.setMessageReaction("🥵", event.messageID,event.threadID)
 
-        		if (event.body.toLowerCase().indexOf("bas2s") !== -1) return api.setMessageReaction("😳", event.messageID,event.threadID)
+        		if (event.body.toLowerCase().indexOf("❤️") !== -1) return api.setMessageReaction("💚", event.messageID,event.threadID)
 
-        		if (event.body.toLowerCase().indexOf("bastog") !== -1) return api.setMessageReaction("😳", event.messageID,event.threadID)
+        		if (event.body.toLowerCase().indexOf("🖤") !== -1) return api.setMessageReaction("😽", event.messageID,event.threadID)
 
-        		if (event.body.toLowerCase().indexOf("hi") !== -1) return api.setMessageReaction("💗", event.messageID,event.threadID)
+        		if (event.body.toLowerCase().indexOf("hi") !== -1) return api.setMessageReaction("🙈", event.messageID,event.threadID)
 
-        		if (event.body.toLowerCase().indexOf("hello") !== -1) return api.setMessageReaction("💗", event.messageID,event.threadID)
+        		if (event.body.toLowerCase().indexOf("hello") !== -1) return api.setMessageReaction("🙊", event.messageID,event.threadID)
 
-        		if (event.body.toLowerCase().indexOf("zope") !== -1) return api.setMessageReaction("⏳", event.messageID,event.threadID)
+        		if (event.body.toLowerCase().indexOf("time") !== -1) return api.setMessageReaction("⏳", event.messageID,event.threadID)
 
-    if (event.body.toLowerCase().indexOf("pangit") !== -1) return api.setMessageReaction("😠", event.messageID,event.threadID)
+    if (event.body.toLowerCase().indexOf("bal") !== -1) return api.setMessageReaction("😠", event.messageID,event.threadID)
 
-    if (event.body.toLowerCase().indexOf("redroom") !== -1) return api.setMessageReaction("😏", event.messageID,event.threadID)
+    if (event.body.toLowerCase().indexOf("privet") !== -1) return api.setMessageReaction("😙", event.messageID,event.threadID)
 
     if (event.body.toLowerCase().indexOf("😏") !== -1) return api.setMessageReaction("😏", event.messageID,event.threadID)
 
-    if (event.body.toLowerCase().indexOf("pakyu") !== -1) return api.setMessageReaction("🤬", event.messageID,event.threadID)
+    if (event.body.toLowerCase().indexOf("😎") !== -1) return api.setMessageReaction("😏", event.messageID,event.threadID)
 
     if (event.body.toLowerCase().indexOf("fuck you") !== -1) return api.setMessageReaction("🤬", event.messageID,event.threadID)
 
-    if (event.body.toLowerCase().indexOf("bata") !== -1) return api.setMessageReaction("👧", event.messageID,event.threadID)
+    if (event.body.toLowerCase().indexOf("fck") !== -1) return api.setMessageReaction("😆", event.messageID,event.threadID)
 
     if (event.body.toLowerCase().indexOf("kid") !== -1) return api.setMessageReaction("👧", event.messageID,event.threadID)
 
