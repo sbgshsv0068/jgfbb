fa1e09dad7834ee085a9b2932acf53af0ffa0178
MOMAMMAD RANA <105457666+TOXIC-RANA@users.noreply.github.com>
2025-04-05 15:47:38 +0600

    Update and rename pair4.js to pair3.js

--- scripts/cmds/pair3.js
+++ scripts/cmds/pair3.js
@@ -4,10 +4,10 @@ const fs = require("fs-extra");
 
 module.exports = {
   config: {
-    name: "pair4",
+    name: "pair3",
     countDown: 5,
     role: 0,
-    category: "fun",
+    category: "love",
     shortDescription: {
       en: "",
     },
