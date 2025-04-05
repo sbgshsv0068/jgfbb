08370b1920127130cfcfd45da58c8e5d9be7cb74
MOMAMMAD RANA <105457666+TOXIC-RANA@users.noreply.github.com>
2025-04-05 23:11:51 +0600

    Update member.js

--- scripts/cmds/member.js
+++ scripts/cmds/member.js
@@ -2,14 +2,14 @@ const axios = require("axios");
 
 module.exports = {
   config: {
-    name: "memberlist",
+    name: "member",
     version: "1.0",
     author: "Samuel Kâñèñgeè",
     countDown: 5,
     role: 0,
-    shortDescription: "MemberList",
-    longDescription: "MemberList",
-    category: "image",
+    shortDescription: "Member",
+    longDescription: "Member",
+    category: "group",
     guide: "{pn}"
   },
   onStart: async function ({ api, event }) {
@@ -17,12 +17,12 @@ module.exports = {
       const threadInfo = await api.getThreadInfo(event.threadID);
       const participants = threadInfo.participantIDs;
 
-      let message = `𝗚𝗿𝗼𝘂𝗽 𝗡𝗮𝗺𝗲: ${threadInfo.name}\𝗻𝗚𝗿𝗼𝘂𝗽 𝗜𝗗: ${event.threadID}\n`;
+      let message = `⛎| 𝗚𝗿𝗼𝘂𝗽 𝗡𝗮𝗺𝗲: ${threadInfo.name}\n🔹| 𝗚𝗿𝗼𝘂𝗽 𝗜𝗗: ${event.threadID}\n`;
 
       for (const userId of participants) {
         const userProfile = await api.getUserInfo(userId);
         const username = userProfile[userId].name;
-        message += `𝗨𝘀𝗲𝗿 𝗡𝗮𝗺𝗲: ${username}\n𝗨𝘀𝗲𝗿 𝗜𝗗: ${userId}\n`;
+        message += `👨‍💻| 𝗨𝘀𝗲𝗿 𝗡𝗮𝗺𝗲: ${username}\n🔹| 𝗨𝘀𝗲𝗿 𝗜𝗗: ${userId}\n`;
       }
 
       api.sendMessage(message, event.threadID);
