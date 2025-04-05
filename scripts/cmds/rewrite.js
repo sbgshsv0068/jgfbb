ad902c259c34fc80da5174333bac70ff87fa6f95
MOMAMMAD RANA <105457666+TOXIC-RANA@users.noreply.github.com>
2025-04-05 22:24:54 +0600

    Update and rename coverfb.js to rewrite.js

---
+++ scripts/cmds/rewrite.js
@@ -0,0 +1,39 @@
+module.exports = {
+	config: {
+		name: "rewrite",
+		version: "1.2",
+		author: "RANA", //Don't change the credit because I made it. Any problems to contact me. https://facebook.com/100063487970328
+		role: 0,
+		shortDescription: "Rewrite a bot's message",
+		longDescription: "Rewrite a bot's message by replying to it with 'edit <message>'.",
+		category: "user",
+		guide: {
+			en: "{p}{n} <message>",
+		},
+	},
+
+	onStart: async function ({ api, event, args }) {
+		if (!event.messageReply || !event.messageReply.messageID) {
+			api.sendMessage("Please reply to a bot's message to rewrite it.", event.threadID, event.messageID);
+			return;
+		}
+
+		const newMessage = args.join(" ");
+		if (!newMessage) {
+			api.sendMessage("Please provide the new message content.", event.threadID, event.messageID);
+			return;
+		}
+
+		try {
+			await api.unsendMessage(event.messageReply.messageID);
+
+			
+			api.sendMessage(newMessage, event.threadID, (err, messageInfo) => {
+				if (err) console.error("Error sending new message:", err);
+			});
+		} catch (error) {
+			console.error("Error editing message:", error);
+			api.sendMessage("Failed to rewrite the message. Make sure the bot has permission and try again.", event.threadID, event.messageID);
+		}
+	},
+};
