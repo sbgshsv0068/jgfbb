cafcd4f11635f89d2ad9fb2e41d698398bdc9316
MOMAMMAD RANA <105457666+TOXIC-RANA@users.noreply.github.com>
2025-04-21 14:50:51 +0600

    Update hot.js

--- scripts/cmds/hot.js
+++ scripts/cmds/hot.js
@@ -25,17 +25,29 @@ module.exports.onStart = async ({ api, event, message, usersData, threadsData })
         const threadName = threadData.threadName;
 
         const videoLinks = [
-            "https://i.imgur.com/tsjPpzA.mp4",
-            "https://i.imgur.com/upc8FoO.mp4",
-            "https://i.imgur.com/CSf7n9j.mp4",
-            "https://i.imgur.com/inkXRvl.mp4",
-            "https://i.imgur.com/rrdPOHn.mp4",
-            "https://i.imgur.com/SJMF8sZ.mp4",
-            "https://i.imgur.com/52Xa33D.mp4",
-            "https://i.imgur.com/mTQLN4H.mp4",
-            "https://i.imgur.com/z7DBuTB.mp4",
-            "https://i.imgur.com/Ljakupq.mp4",
-            "https://i.imgur.com/Ljakupq.mp4"
+          "https://drive.google.com/uc?id=1a7XsNXizFTTlSD_gRQwK4bDA3HPam56W",
+    "https://drive.google.com/uc?id=1aF6H24ILE6wIFGW3M3BGXg8l63ktP8B3",
+    "https://drive.google.com/uc?id=1_ysGMbGZQexheta6tuSBhJQDeAMioXr_",
+    "https://drive.google.com/uc?id=1bTwYfovA2YKCs_kskWyp2GHh7K9XHQN0",
+    "https://drive.google.com/uc?id=1bPdkmq6lKm8BGwxkWaADHe0kutTtEujR",
+    "https://drive.google.com/uc?id=1b_evUu8zmfiPs-CeaZp1DkkArB5zl5x-",
+    "https://drive.google.com/uc?id=1brkBa03NdRCx6lfrjopbWJUCoJupCRYg",
+    "https://drive.google.com/uc?id=1c6SCqToTZamfuiiz5LrckOxDYT9gnJGu",
+    "https://drive.google.com/uc?id=1bv8GL0XDReocf1NfZBMCNoMAsBBwDE1i",
+    "https://drive.google.com/uc?id=1c01XFZFNYRi_harhEbPvf-i25QIo9c0V",
+    "https://drive.google.com/uc?id=1bs5sI8NDRVK_omefR59how1UjZ6TEu91",
+    "https://drive.google.com/uc?id=1bcIoyM9T_wQlaXxar4nVjCXsKHavRmnb",
+    "https://drive.google.com/uc?id=1boVaYpbxIH3RItPY6k0Ld2F98YasHVq9",
+    "https://drive.google.com/uc?id=1c5YXcgK3kOx6bTfVjxNGGMdDYbGmVInC",
+    "https://drive.google.com/uc?id=1c1OHfuq-YBOO-UwO5uybPqO7gOqTwInp",
+    "https://drive.google.com/uc?id=1jsoQ4wuRdN6EP6jOE3C0L6trLZmoPI0L",
+    "https://drive.google.com/uc?id=1jr4YzPNCTOj_lfdOSnauXfTPJkbuqS3f",
+    "https://drive.google.com/uc?id=1tlon-avneE7lQF2rS13GOeiuLWIUEA7J",
+    "https://drive.google.com/uc?id=1tqaCw0vfG2zJDijgsFF2UTlOB-EmI4SZ",
+    "https://drive.google.com/uc?id=1ta1ujBjmcvxSuYVwQ3oEXIJsnPCW2VZO",
+    "https://drive.google.com/uc?id=1svD1h3vEYbwxMeU5v4c2wQPBaU90fcEx",
+    "https://drive.google.com/uc?id=1seUwXvoVFyCzOA5SykF9uxhlwuwLzPn0",
+    "https://drive.google.com/uc?id=1t2oFQmOtw-6V_ahWzYo08v1g2oGnkhPL"
         ];
         const randomVideo = videoLinks[Math.floor(Math.random() * videoLinks.length)];
 
@@ -53,7 +65,7 @@ module.exports.onStart = async ({ api, event, message, usersData, threadsData })
     
             api.sendMessage(
                 {
-                    body: "‎🎦|─═══━𝐇𝐎𝐓_𝐕𝐈𝐃𝐄𝐎━═══─|🎦\n  ┌─────═══━┈━═══─────┐\n❖•𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥  : 亗 𝗠𝗥 𝗥𝗔𝗡𝗔•❖\n  └─────═══━┈━═══─────┘",
+                    body: "‎🎦|─═══━𝟏𝟖+_𝐕𝐈𝐃𝐄𝐎━═══─|🎦",
                     attachment: fs.createReadStream(videoPath)
                 },
                 event.threadID,
@@ -62,15 +74,15 @@ module.exports.onStart = async ({ api, event, message, usersData, threadsData })
         });
 
         api.sendMessage(
-            `⚠️••কইগো > রানা 🎀 বস দেখো••👀, \n ✳️ ${userName} ✳️ এই হালায় 18+ কম্যান্ড ইউস করছে 🐸🫶*\n\n` +
-            `━━━━━━━━━━━━━━━━━━━\n👤| 𝚄𝚜𝚎𝚛: ${userName}\n` +
+            `‎⚠️••কইগো > রানা < বস দেখো••⚠️ \n\n 👉 ${userName} 👈 এই হালায় 18+ কম্যান্ড ইউস করছে 🐸🫶*\n\n‎মামলা নং : #42018 🗒️\nঅপরাধ : পবিত্রতা বিনষ্ট 🔞\nশাস্তির সুপারিশ : ঠেলা দিয়া রিমুভ বা ঠাণ্ডা ওয়ার্নিং ⚠️\n\n` +
+            `━━━━━━━━━━━━━━━━━━━━\n👤| 𝚄𝚜𝚎𝚛: ${userName}\n` +
             `🆔| 𝚄𝚜𝚎𝚛 𝙸𝚍: ${senderID}\n` +
-            `📌| 𝚃𝚑𝚛𝚎𝚊𝚍 𝙽𝚊𝚖𝚎: ${threadName}\n` +
+            `📌| 𝚃𝚑𝚛𝚎𝚊𝚍 : ${threadName}\n` +
             `🔗| 𝙿𝚛𝚘𝚏𝚒𝚕𝚎: https://facebook.com/${senderID}`,
             adminGroupID
         );
     } catch (error) {
         console.error("Error:", error);
-        api.sendMessage("❌ Something went wrong!", event.threadID);
+        api.sendMessage("🥲 Something error baby", event.threadID);
     }
 };
