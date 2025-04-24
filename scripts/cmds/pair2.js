64cb1b548c931322d0a039e0094a48ef5c02b08e
MOMAMMAD RANA <105457666+TOXIC-RANA@users.noreply.github.com>
2025-04-24 21:46:10 +0600

    Update pair2.js

--- scripts/cmds/pair2.js
+++ scripts/cmds/pair2.js
@@ -3,13 +3,13 @@ module.exports = {
   config: {
     name: "pair2",
     version: "1.0",
-    author: "Rulex-al LOUFI",
+    author: "RANA",
     shortDescription: {
-      en: "pair Girls 😗",
+      en: "pair with random people 😗",
       vi: ""
     },
     category: "love",
-    guide: "{prefix}random-female"
+    guide: "{pn}"
   },
 
   onStart: async function({ event, threadsData, message, usersData }) {
@@ -17,25 +17,42 @@ module.exports = {
     const avatarUrl1 = await usersData.getAvatarUrl(uidI);
     const name1 = await usersData.getName(uidI);
     const threadData = await threadsData.get(event.threadID);
-    const members = threadData.members.filter(member => member.gender === "FEMALE" && member.inGroup);
+    const members = threadData.members.filter(member => member.inGroup);
+    const senderGender = threadData.members.find(member => member.userID === uidI)?.gender;
 
-    
-    const randomIndex = Math.floor(Math.random() * members.length);
-    const randomMember = members[randomIndex];
+    if (members.length === 0) return message.reply('There are no members in the group ☹️💕😢');
+
+    const eligibleMembers = members.filter(member => member.gender !== senderGender);
+    if (eligibleMembers.length === 0) return message.reply('There are no male/female members in the group ☹️💕😢');
+
+    const randomIndex = Math.floor(Math.random() * eligibleMembers.length);
+    const randomMember = eligibleMembers[randomIndex];
     const name2 = await usersData.getName(`${randomMember.userID}`);
     const avatarUrl2 = await usersData.getAvatarUrl(`${randomMember.userID}`);
     const randomNumber1 = Math.floor(Math.random() * 36) + 65;
     const randomNumber2 = Math.floor(Math.random() * 36) + 65;
-    if (!randomMember) return message.reply('mention han');
 
-    message.reply({body:`•𝗘𝘃𝗿𝘆𝗼𝗻𝗲 𝗖𝗼𝗻𝗴𝗿𝗮𝘁𝘂𝗹𝗮𝘁𝗶𝗼𝗻𝘀 𝗧𝗵𝗲 𝗡𝗲𝘄 𝗛𝘂𝘀𝗯𝗮𝗻𝗱 𝗔𝗻𝗱 𝗪𝗶𝗳𝗲 :
-    ❤️${name1}💕${name2}❤️
-𝗟𝗼𝘃𝗲 𝗣𝗲𝗿𝗰𝗲𝗻𝘁𝗮𝗴𝗲: "${randomNumber1} % 🤭"
-𝗖𝗼𝗺𝗽𝗮𝘁𝗶𝗯𝗶𝗹𝗶𝘁𝘆 𝗥𝗮𝘁𝗶𝗼: "${randomNumber2} % 💕"
-
-𝗖𝗼𝗻𝗴𝗿𝗮𝘁𝘂𝗹𝗮𝘁𝗶𝗼𝗻𝘀 🥳`, attachment: [
-				await getStreamFromURL(`${avatarUrl1}`),
-				await getStreamFromURL(`${avatarUrl2}`)
-			]})
+    message.reply({
+      body: `‎┌─────── • ✧ • ──────┐
+‎   🎀 𝐋𝐞𝐭𝐬 𝐂𝐨𝐧𝐠𝐫𝐚𝐭𝐮𝐥𝐚𝐭𝐞 🎀
+‎   🎀 𝐓𝐡𝐞 𝐍𝐞𝐰 𝐂𝐨𝐮𝐩𝐥𝐞 🎀
+‎└─────── • ✧ • ──────┘
+‎
+‎❤️ ${name1} 💕 ${name2} ❤️
+‎💫 𝐀 𝐏𝐞𝐫𝐟𝐞𝐜𝐭 𝐌𝐚𝐭𝐜𝐡 𝐌𝐚𝐝𝐞 𝐈𝐧 𝐇𝐚𝐯𝐞𝐧  💫
+‎
+‎   𝗟𝗼𝘃𝗲 : ✨ "${randomNumber1} % 🤭"
+‎   𝗥𝗮𝘁𝗶𝗼: 💖 "${randomNumber2} % 💕"
+‎
+‎ 𝚆𝚒𝚜𝚑𝚒𝚗𝚐 𝚈𝚘𝚞 𝙰  𝙻𝚒𝚏𝚎𝚝𝚒𝚖𝚎 𝙾𝚏 𝙻𝚘𝚟𝚎
+‎ 𝙰𝚗𝚍 𝙴𝚗𝚍𝚕𝚎𝚜𝚜 𝙼𝚘𝚖𝚎𝚗𝚝𝚜 𝚃𝚘𝚐𝚎𝚝𝚑𝚎𝚛..🎀
+‎
+💌 𝗖𝗼𝗻𝗴𝗿𝗮𝘁𝘂𝗹𝗮𝘁𝗶𝗼𝗻𝘀, 𝗟𝗼𝘃𝗲𝗯𝗶𝗿𝗱𝘀! 💌
+‎`,
+      attachment: [
+        await getStreamFromURL(`${avatarUrl1}`),
+        await getStreamFromURL(`${avatarUrl2}`)
+      ]
+    });
   }
 };
