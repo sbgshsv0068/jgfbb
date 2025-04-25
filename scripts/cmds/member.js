28192b63825ab883c117767c344a3334db98c2b9
MOMAMMAD RANA <105457666+TOXIC-RANA@users.noreply.github.com>
2025-04-26 00:41:55 +0600

    Update member.js

--- scripts/cmds/member.js
+++ scripts/cmds/member.js
@@ -9,7 +9,7 @@ module.exports = {
     role: 0,
     shortDescription: "Member",
     longDescription: "Member",
-    category: "group",
+    category: "box chat",
     guide: "{pn}"
   },
   onStart: async function ({ api, event }) {
@@ -17,12 +17,12 @@ module.exports = {
       const threadInfo = await api.getThreadInfo(event.threadID);
       const participants = threadInfo.participantIDs;
 
-      let message = `⛎| 𝗚𝗿𝗼𝘂𝗽 𝗡𝗮𝗺𝗲: ${threadInfo.name}\n🔹| 𝗚𝗿𝗼𝘂𝗽 𝗜𝗗: ${event.threadID}\n`;
+      let message = `‎┌─────── • ✧ • ──────┐\n⛎| 𝗚𝗰 𝗡𝗮𝗺𝗲: ${threadInfo.name}\n🔹| 𝗚𝗰 𝗜𝗗: ${event.threadID}‎\n‎└─────── •✧ • ──────┘\n\n`;
 
       for (const userId of participants) {
         const userProfile = await api.getUserInfo(userId);
         const username = userProfile[userId].name;
-        message += `👨‍💻| 𝗨𝘀𝗲𝗿 𝗡𝗮𝗺𝗲: ${username}\n🔹| 𝗨𝘀𝗲𝗿 𝗜𝗗: ${userId}\n`;
+        message += `👤| 𝗨𝘀𝗲𝗿 𝗡𝗮𝗺𝗲: ${username}\n🆔| 𝗨𝘀𝗲𝗿 𝗜𝗗: ${userId}\n───────────────────\n`;
       }
 
       api.sendMessage(message, event.threadID);
