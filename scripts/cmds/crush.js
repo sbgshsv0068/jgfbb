830d7d9389a08ce2989c7a9b5dee17e46e1939cb
MOMAMMAD RANA <105457666+TOXIC-RANA@users.noreply.github.com>
2025-04-25 22:28:38 +0600

    Update crush.js

--- scripts/cmds/crush.js
+++ scripts/cmds/crush.js
@@ -1,49 +1,82 @@
-const { GoatWrapper } = require("fca-liane-utils");
- module.exports = {
+module.exports = {
   config: {
-    name: "crush",
-    version: "7.0",
-    author: "Vex_Arafat",
-    countDown: 5,
+    name: "anime",
+    aliases: ["animevideo", "anivideo"],
+    version: "1.0",
+    author: "kshitiz",
+    countDown: 20,
     role: 0,
-    shortDescription: "all video ðŸ“·",
-    longDescription: "",
-    category: "MEDIA",
-    guide: "{pn}"
+    shortDescription: "get anime video",
+    longDescription: "get random anime video",
+    category: "video",
+    guide: "{pn} animevdo",
   },
-   onStart: async function ({ message }) {
-   var TARIF= ["https://i.imgur.com/h2LdMXo.mp4",
-"https://i.imgur.com/UBGhUg9.mp4",
-"https://i.imgur.com/dcneJDj.mp4",
-"https://i.imgur.com/Q1mN3kh.mp4",
-"https://i.imgur.com/axpoccu.mp4",
-"https://i.imgur.com/g83JXzX.mp4",
-"https://i.imgur.com/kP59wkj.mp4",
-"https://i.imgur.com/Zjjc4Jq.mp4",
-"https://i.imgur.com/gE2i22B.mp4",
-"https://i.imgur.com/n3f3bpd.mp4",
-"https://i.imgur.com/eYcAl58.mp4",
- "https://i.imgur.com/LOJJReh.mp4",
-"https://i.imgur.com/vWR0hS3.mp4",
-"https://i.imgur.com/vWR0hS3.mp4",
-"https://i.imgur.com/aRux5BV.mp4",
-"https://i.imgur.com/qZ4KrvM.mp4",
-"https://i.imgur.com/t10ATBv.mp4",
-"https://i.imgur.com/86jOvKQ.mp4",
-"https://i.imgur.com/sRyzonR.mp4",
-"https://i.imgur.com/Tuq9F7B.mp4",
-"https://i.imgur.com/Xc2e4z7.mp4",
-"https://i.imgur.com/HHj5RHm.mp4",
-"https://i.imgur.com/o0buUIW.mp4",
-"https://i.imgur.com/ogXI3GY.mp4",
-"https://i.imgur.com/CxdF8A9.mp4",
-]
 
-let msg = TARIF[Math.floor(Math.random()*TARIF.length)]
-message.send({
-  body: '	🗂️───═══━𝐂𝐫𝐮𝐬𝐡━═══────		┌─────═══━┈━═══─────┐			🪫:𝙊𝙒𝙉𝙀𝙍 𝘿𝙀𝙑 : 亗 ɱʀ ʀʌnʌ 亗 └─────═══━┈━═══─────┘',attachment: await global.utils.getStreamFromURL(msg)
-})
-}
-     }
-const wrapper = new GoatWrapper(module.exports);
-wrapper.applyNoPrefix({ allowPrefix: true });
+  sentVideos: [],
+
+  onStart: async function ({ api, event, message }) {
+    const senderID = event.senderID;
+
+    const loadingMessage = await message.reply({
+      body: "𝘓𝘰𝘢𝘥𝘪𝘯𝘨 𝘈𝘯𝘪𝘮𝘦 𝘝𝘪𝘥𝘦𝘰.. 𝘗𝘭𝘦𝘢𝘴𝘦 𝘞𝘢𝘪𝘵 ! ⏳",
+    });
+
+    const link = [
+      "https://drive.google.com/uc?export=download&id=1cyB6E3z4-_Dr4mlYFB87DlWkUlC_KvrR",// video credits xenoz (youtube)
+      "https://drive.google.com/uc?export=download&id=1Q5L8SGKYpNrXtJ6mffcwMA9bcUtegtga",
+      "https://drive.google.com/uc?export=download&id=1u8JzKCTubRhnh0APo2mMob-mQM0CoNYj",
+      "https://drive.google.com/uc?export=download&id=1JBIo966g0MmUT27S1yc0B06lASt4dD9V",
+      "https://drive.google.com/uc?export=download&id=1w_HUyAFHnVfkUl8XLY01pxs8dnmQNEVn",
+      "https://drive.google.com/uc?export=download&id=1EoeMITZrSNB1PpPjsh5cmsFzbjMZKH2c",
+      "https://drive.google.com/uc?export=download&id=1Kh4qvle57FlMjcam-JNxTQtPZe2uxrJ8",
+      "https://drive.google.com/uc?export=download&id=1KtyLzqbyJpq5_ke0Cb6gD89ZNf0NQm0t",
+      "https://drive.google.com/uc?export=download&id=1vy0ZldnlTqXgwJ36HxOXC9hLObgNkTZ-",
+      "https://drive.google.com/uc?export=download&id=1hPZhzKm_uj6HRsEdFAH1lPFFF8vC-lTB",
+      "https://drive.google.com/uc?export=download&id=1AJCeDc-MvtvSspz7oX98ywzDB3Z29bSu",
+      "https://drive.google.com/uc?export=download&id=1reVD_c5kK29iTdLAu_7sYFBB0hzrRkAx",
+      "https://drive.google.com/uc?export=download&id=1vmnlCwp40mmjW6aFob_wD_U1PmOgRYst",
+      "https://drive.google.com/uc?export=download&id=1R0n8HQgMEEAlaL6YJ3JiDs_6oBdsjN0e",
+      "https://drive.google.com/uc?export=download&id=1tUJEum_tf79gj9420mHx-_q7f0QP27DC",
+      "https://drive.google.com/uc?export=download&id=1hAKRt-oOSNnUNYjDQG-OF-tdzN_qJFoR",
+      "https://drive.google.com/uc?export=download&id=1HrvT5jaPsPi66seHCLBkRbTziXJUkntn",
+      "https://drive.google.com/uc?export=download&id=1v8k2YxBme5zEumlNiLIry5SDMryfkBts",
+      "https://drive.google.com/uc?export=download&id=1x01XDJoJMbtUjWztomF25Ne1Up4cWQoC",
+      "https://drive.google.com/uc?export=download&id=12j65dstfkMUHMSmQU8FnZi2RyHPHJipx",
+      "https://drive.google.com/uc?export=download&id=13ImpZl3aLHpwlYhWvjKLfiRvFsK3kl5z",
+      "https://drive.google.com/uc?export=download&id=1EdFmtprVtt652PDocRlgeXXxIQRYTSQw",
+      "https://drive.google.com/uc?export=download&id=1EdFmtprVtt652PDocRlgeXXxIQRYTSQw",
+      "https://drive.google.com/uc?export=download&id=1QdLGspkvM-Gf1SHh2fJf8zPbrZaURTJs",
+      "https://drive.google.com/uc?export=download&id=1RyG2Lh1cp6lq9IEIr4vVaDyu21RW_pav",
+      "https://drive.google.com/uc?export=download&id=1zlmaoBVrk9GKPZ_2XYZzzQkFMdiszSzL",
+      "https://drive.google.com/uc?export=download&id=1rcxnb5U4gnwSiZhOcsbahqzE003LKYXc",
+      "https://drive.google.com/uc?export=download&id=12cjBYkdDR4BMKj1H4aV6rfa7sVuoU3eU",
+      "https://drive.google.com/uc?export=download&id=1aBHnJ7AgkQKC9RBIycVN-l6F4kdeX3hf",
+      "https://drive.google.com/uc?export=download&id=13X4yhx9Nr8tIleXtxC7bV1Rfjt1FXeDv",
+      "https://drive.google.com/uc?export=download&id=1uuajuhhLPlLXlSRBdzmwGfIMAV6WwW5u",
+      "https://drive.google.com/uc?export=download&id=1wkoC5kbo4GuDEqoEXoz40DwZi6OMKiSI",
+      // Add more video links here
+    ];
+
+    const availableVideos = link.filter(video => !this.sentVideos.includes(video));
+
+    if (availableVideos.length === 0) {
+      this.sentVideos = [];
+    }
+
+    const randomIndex = Math.floor(Math.random() * availableVideos.length);
+    const randomVideo = availableVideos[randomIndex];
+
+    this.sentVideos.push(randomVideo);
+
+    if (senderID !== null) {
+      message.reply({
+        body: '𝘏𝘢𝘳𝘦 𝘠𝘰𝘶𝘳 𝘈𝘯𝘪𝘮𝘦 𝘝𝘪𝘥𝘦𝘰..☑️',
+        attachment: await global.utils.getStreamFromURL(randomVideo),
+      });
+
+      setTimeout(() => {
+        api.unsendMessage(loadingMessage.messageID);
+      }, 5000);
+    }
+  },
+};
